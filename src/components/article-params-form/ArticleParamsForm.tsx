ddba5b9ad4d0464b2d687f282ff9d94d1ea25160
Anastasia Veisa <anveysa@yandex.ru>
2024-06-12 16:37:35 +0400

    add handleOutsideClick for sidebar

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,11 +2,28 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
-import { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
 	const [isOpen, setOpen] = useState<boolean>(false);
+	const formRef = useRef<HTMLDivElement>(null);
+
+	// закрытие сайдбара по клику вне его области
+	useEffect(() => {
+		const handleOutsideClick = (event: MouseEvent) => {
+			// проверяем, произошел ли клик вне сайдбара
+			if (formRef.current && !formRef.current.contains(event.target as Node)) {
+				setOpen(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleOutsideClick);
+		return () => {
+			document.removeEventListener('mousedown', handleOutsideClick);
+		};
+	}, []);
+
 	return (
 		<>
 			<ArrowButton
@@ -16,6 +33,7 @@ export const ArticleParamsForm = () => {
 				}}
 			/>
 			<aside
+				ref={formRef}
 				className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
