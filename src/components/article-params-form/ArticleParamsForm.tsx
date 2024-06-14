1be4b8ae7044d31595ae72afb33923c80d167f82
Anastasia Veisa <anveysa@yandex.ru>
2024-06-14 14:23:04 +0400

    add selects and radio group to form

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,18 +1,38 @@
+import React, { FormEvent, useEffect, useRef, useState } from 'react';
+import clsx from 'clsx';
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { Text } from 'components/text';
+import { Select } from 'components/select';
+import { Separator } from 'components/separator';
+import { RadioGroup } from 'components/radio-group';
+
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontSizeOptions,
+	ArticleStateType,
+	OptionType,
+} from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
-import React, { useEffect, useRef, useState } from 'react';
-import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsProps = {
+	params: ArticleStateType;
+	onSubmit: (params: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({ params, onSubmit }: ArticleParamsProps) => {
+	const formRef = useRef<HTMLFormElement>(null);
+
 	const [isOpen, setOpen] = useState<boolean>(false);
-	const formRef = useRef<HTMLDivElement>(null);
+	const [formState, setFormState] = useState<ArticleStateType>(params);
 
-	// закрытие сайдбара по клику вне его области
 	useEffect(() => {
 		const handleOutsideClick = (event: MouseEvent) => {
-			// проверяем, произошел ли клик вне сайдбара
 			if (formRef.current && !formRef.current.contains(event.target as Node)) {
 				setOpen(false);
 			}
@@ -24,20 +44,69 @@ export const ArticleParamsForm = () => {
 		};
 	}, []);
 
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+	};
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		onSubmit(formState);
+		setOpen(false);
+	};
+
+	const handleSelectChange =
+		(key: keyof ArticleStateType) => (value: OptionType) => {
+			setFormState((prevState) => ({
+				...prevState,
+				[key]: value,
+			}));
+		};
+
 	return (
 		<>
-			<ArrowButton
-				isOpen={isOpen}
-				handleClick={() => {
-					setOpen(!isOpen);
-				}}
-			/>
+			<ArrowButton isOpen={isOpen} handleClick={() => setOpen(!isOpen)} />
 			<aside
-				ref={formRef}
 				className={clsx(styles.container, isOpen && styles.container_open)}>
-				<form className={styles.form}>
+				<form className={styles.form} ref={formRef} onSubmit={handleSubmit}>
+					<Text size={31} weight={800} uppercase={true}>
+						Задайте параметры
+					</Text>
+					<div className={styles.wrapper}>
+						<Select
+							selected={formState.fontFamilyOption}
+							options={fontFamilyOptions}
+							onChange={handleSelectChange('fontFamilyOption')}
+							title='Шрифт'
+						/>
+						<RadioGroup
+							name='fontSize'
+							options={fontSizeOptions}
+							selected={formState.fontSizeOption}
+							onChange={handleSelectChange('fontSizeOption')}
+							title='Размер шрифта'
+						/>
+						<Select
+							selected={formState.fontColor}
+							options={fontColors}
+							onChange={handleSelectChange('fontColor')}
+							title='Цвет шрифта'
+						/>
+						<Separator />
+						<Select
+							selected={formState.backgroundColor}
+							options={backgroundColors}
+							onChange={handleSelectChange('backgroundColor')}
+							title='Цвет фона'
+						/>
+						<Select
+							selected={formState.contentWidth}
+							options={contentWidthArr}
+							onChange={handleSelectChange('contentWidth')}
+							title='Ширина контента'
+						/>
+					</div>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
+						<Button title='Сбросить' type='reset' onClick={handleReset} />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
